0272918b2d7137f6ffedf19bb993bf83de0e9312
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2023-12-30 14:56:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -696,7 +696,6 @@ app.get('/', (req, res) => {
 	<!-- footer end -->
 
 	 <script src="./js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 </html>
 `
