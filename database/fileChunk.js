f981f2f134d19c0d799a3a4f424c28a5b030ffc2
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2024-06-21 16:01:49 +0200

    Create fileChunk.js

---
+++ database/fileChunk.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const chunkSchema = new mongoose.Schema({
+  key: String,
+  data: Array,
+  mapzz: String,
+  extension: String,
+  url: String,
+  url2: String,
+  size: String,
+  expired: String,
+});
+
+const Chunk = mongoose.model('Chunk', chunkSchema);
+
+module.exports = Chunk;
