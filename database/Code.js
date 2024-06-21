33f2bcc0a243585f6c85c0625909e6e9c250e954
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2024-06-21 16:00:28 +0200

    Create Code.js

---
+++ database/Code.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+const result = new mongoose.Schema({
+event: String || 'Commands',
+name: String,
+cmd: Array,
+code: String || `await message.channel.send('There is an error in the system! please try again later..')`,
+description: String || 'No description',
+userPerms: String || 'Default',
+botPerms: String || 'Default',
+usage: String || 'No usage',
+set: String || 'Enable',
+});
+
+const test = mongoose.model("discord", result);
+
+module.exports = test;
