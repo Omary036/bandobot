63abf30303ca364105a0a0ec2ac64eb2c780e739
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2024-07-09 23:59:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,48 @@ app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 const eventModelz = require('./database/data.js');
 
+const ALL_INTENTS = 
+    (1 << 0) +  // GUILDS
+    (1 << 1) +  // GUILD_MEMBERS
+    (1 << 2) +  // GUILD_BANS
+    (1 << 3) +  // GUILD_EMOJIS_AND_STICKERS
+    (1 << 4) +  // GUILD_INTEGRATIONS
+    (1 << 5) +  // GUILD_WEBHOOKS
+    (1 << 6) +  // GUILD_INVITES
+    (1 << 7) +  // GUILD_VOICE_STATES
+    (1 << 8) +  // GUILD_PRESENCES
+    (1 << 9) +  // GUILD_MESSAGES
+    (1 << 10) + // GUILD_MESSAGE_REACTIONS
+    (1 << 11) + // GUILD_MESSAGE_TYPING
+    (1 << 12) + // DIRECT_MESSAGES
+    (1 << 13) + // DIRECT_MESSAGE_REACTIONS
+    (1 << 14);  // DIRECT_MESSAGE_TYPING
+
+
+const client = new Client({
+	intents: [
+    ALL_INTENTS,
+  	GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages, 
+		GatewayIntentBits.GuildPresences, 
+		GatewayIntentBits.GuildMessageReactions, 
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildBans,
+    
+	], 
+	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent],
+  disableMentions: 'everyone',
+});
+
+
+
+require("dotenv").config();
+
+const config = require('./config.json');
+
+
 client.on('shardError', error => {
 	console.error('A websocket connection encountered an error:', error);
 });
@@ -384,46 +426,6 @@ var envthing;
 
 
 
-const ALL_INTENTS = 
-    (1 << 0) +  // GUILDS
-    (1 << 1) +  // GUILD_MEMBERS
-    (1 << 2) +  // GUILD_BANS
-    (1 << 3) +  // GUILD_EMOJIS_AND_STICKERS
-    (1 << 4) +  // GUILD_INTEGRATIONS
-    (1 << 5) +  // GUILD_WEBHOOKS
-    (1 << 6) +  // GUILD_INVITES
-    (1 << 7) +  // GUILD_VOICE_STATES
-    (1 << 8) +  // GUILD_PRESENCES
-    (1 << 9) +  // GUILD_MESSAGES
-    (1 << 10) + // GUILD_MESSAGE_REACTIONS
-    (1 << 11) + // GUILD_MESSAGE_TYPING
-    (1 << 12) + // DIRECT_MESSAGES
-    (1 << 13) + // DIRECT_MESSAGE_REACTIONS
-    (1 << 14);  // DIRECT_MESSAGE_TYPING
-
-
-const client = new Client({
-	intents: [
-    ALL_INTENTS,
-  	GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages, 
-		GatewayIntentBits.GuildPresences, 
-		GatewayIntentBits.GuildMessageReactions, 
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildBans,
-    
-	], 
-	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent],
-  disableMentions: 'everyone',
-});
-
-
-
-require("dotenv").config();
-
-const config = require('./config.json');
 
 // client.commands = new Collection()
 // client.aliases = new Collection()
