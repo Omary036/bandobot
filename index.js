19aa73b24c3ff3fc087b31170bcc13f9cf2b266f
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2024-08-02 05:06:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const cors = require('cors');
 
 // Use the cors middleware
 //app.use(cors());
-app.use(cors({ origin: '*' }));
+//app.use(cors({ origin: '*' }));
+app.use(cors({
+    origin: '*', // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
