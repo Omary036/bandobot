881cf56fd3ce8548ab6cfadd95e6604c6b5bce0d
Omar Karrom <74334309+Omary036@users.noreply.github.com>
2024-08-02 05:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const eventModel = require('./database/code.js')
 const cors = require('cors');
 
 // Use the cors middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
